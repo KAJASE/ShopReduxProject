b51217a3287e20dcd3c10c9ca6e545c5905ce960
Karen <kareninejapiassu@gmail.com>
2022-06-13 12:50:12 +0200

    Fixing App useSelector

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
-  const showCart = useSelector((state) => state.ui.uiSlice);
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
   return (
     <Layout>
       {showCart && <Cart />}
