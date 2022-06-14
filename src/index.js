13960aa3389cf84242170e30df8db9ff73d6df95
Karen <kareninejapiassu@gmail.com>
2022-06-15 01:48:59 +0200

    Adding a CartButton

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
+import store from './components/store/index';
 import './index.css';
 import App from './App';
-import store from "./store/index";
+
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
