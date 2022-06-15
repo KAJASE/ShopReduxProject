46331704863c4774e8ba1ee1bb49eb04be74194b
Karen <kareninejapiassu@gmail.com>
2022-06-15 13:37:35 +0200

    Fixing total price

--- src/components/store/cart-slice.js
+++ src/components/store/cart-slice.js
@@ -13,7 +13,7 @@ const cartSlice = createSlice({
       state.totalQuantity++; 
       if(!existingItem){
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
@@ -21,7 +21,7 @@ const cartSlice = createSlice({
         });
       }else{
         existingItem.quantity++;
-        existingItem.totalPrice = existingItem.totalPrice + newItem.totalPrice;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
     removeItemFromCart(state, action){
